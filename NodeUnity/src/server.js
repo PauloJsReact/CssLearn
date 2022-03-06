3fdbb1f7f818b3c0e86890f1fa25220d131a2cc7
PAULO HENRIQUE <paulo_henrique_araujo@outlook.com>
2022-03-06 00:06:25 -0300

    The server as update

--- NodeUnity/src/server.js
+++ NodeUnity/src/server.js
@@ -4,6 +4,15 @@ const bodyParser = require("body-parser");
 const app = express();
 const port ="3000";
 
+app.get('/',function(req,res,next) {
+    res.status(200).send("hello word!!");
+});
+
+app.get('/player',function (req,res,next) {
+    res.status(200).send("<h1>Players!</h1>")
+})
+
+
 app.listen(port,function(){
     console.log(`Server listering on port ${port} ...`);
 })
