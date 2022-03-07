210c8f7491728d5915875627a368abe77b4ef1d7
PAULO HENRIQUE <paulo_henrique_araujo@outlook.com>
2022-03-07 07:00:33 -0300

    http method created

--- NodeUnity/src/server.js
+++ NodeUnity/src/server.js
@@ -8,10 +8,23 @@ app.get('/',function(req,res,next) {
     res.status(200).send("hello word!!");
 });
 
+
+//player metodo
 app.get('/player',function (req,res,next) {
-    res.status(200).send("<h1>Players!</h1>")
+    res.status(200).send("<h1>GET</h1>")
 })
 
+app.post('/player', function (req,res,next) {
+    res.status(200).send("<h1>POST</h1>");
+});
+
+app.put('/player',function(req,res,next) {
+    res.status(201).send("<h1>PUT</h1>");
+})
+
+app.delete('/player',function(req,res,next) {
+    res.status(200).send("<h1>DELETE</h1>")
+})
 
 app.listen(port,function(){
     console.log(`Server listering on port ${port} ...`);
